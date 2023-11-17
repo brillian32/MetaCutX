fe8748e6d437f94e65e8c7917907bf7ac75c1ccc
brillian <1730505961@qq.com>
2023-11-18 02:17:03 +0800

    feat:增加单元测试

---
+++ Core/MXRender/test/MXRenderTest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by brillan on 2023/11/18.
+//
+
+//#include "MXSpdLog.h"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include "DecodeVideo.h"
+
+
+TEST_CASE("Decode Video") {
+	DecodeVideo decode("/Users/brillan/Desktop/xingshan.mp4");
+	decode.setOutputFilePath("/Users/brillan/Desktop/1");
+	decode.decodeVideo();
+}
--- main.cpp
+++ main.cpp
@@ -2,18 +2,13 @@
 #include <MXRenderTest.h>
 #include <QCoreApplication>
 #include "DecodeVideo.h"
-#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
-	spdlog::set_level(spdlog::level::info);
-	QCoreApplication app(argc, argv);
-	std::cout << "Hello, World!" << std::endl;
 	MXRenderTest test;
 	test.test();
 	DecodeVideo decode("/Users/brillan/Desktop/xingshan.mp4");
 	decode.setOutputFilePath("/Users/brillan/Desktop/1");
 	decode.decodeVideo();
-//	decode.decodeVideo();
 	return 0;
 }
